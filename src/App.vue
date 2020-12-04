d7c7ff6df453812b217df44e4f5a9293b2cb88f0
miladMohammadi <afraaliansite@gmail.com>
2020-12-04 05:53:39 -0800

    commit4

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <header-section></header-section>
+  <slider-vue></slider-vue>
 </template>
 
 <script>
 import headerSection from './components/header'
+import sliderVue from './components/slider.vue'
+
 
 export default {
   name: 'App',
   components: {
-    headerSection
+    headerSection,
+    sliderVue
   }
 }
 </script>
---
+++ src/components/slider.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="banner-arias" id="banner-aria">
+        <div class="container">
+            <div class="swiper-container-img">
+                    <!-- Additional required wrapper -->
+                <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <div class="swiper-slide"><img src="./images/image-slider/banner1.jpg" alt=""></div>
+                        <div class="swiper-slide"><img src="./images/image-slider/banner2.jpg" alt=""></div>
+                        <div class="swiper-slide"><img src="./images/image-slider/banner3.jpg" alt=""></div>
+                    <!-- Add Pagination -->
+                    <div class="swiper-pagination "></div>
+                    <!-- Add Arrows -->
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
