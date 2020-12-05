7c45a24539362460de98ecc6c5921347afe28b19
miladMohammadi <afraaliansite@gmail.com>
2020-12-04 21:38:30 -0800

    load to file app. vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <header-section></header-section>
-  
+  <main-section></main-section>
 </template>
 
 <script>
 import headerSection from './components/header';
-
+import mainSection from './components/main';
 
 export default {
   name: 'App',
   components: {
     headerSection,
+    mainSection
   }
 }
 </script>
