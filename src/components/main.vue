c73496e9e40bd5c13ce0c2afe23d9be440775dc6
miladMohammadi <afraaliansite@gmail.com>
2020-12-07 09:20:36 -0800

    complate

--- src/components/main.vue
+++ src/components/main.vue
@@ -392,139 +392,14 @@
         </section>
         <!-- End section blog -->
 
-        <!-- Start Footer -->
-    <div class="footer">
-        <div class="container">
-            <div class="items-main-footer">
-                <section class="item1">
-                    <p>مطالب پربازدید</p>
-                    <ul>
-                        <li>
-                            <a href="">
-                                <i class="fal fa-chevron-left"></i>
-                                عسل آویشن و خواص آن
-                            </a>
-                        </li>
-                        <li>
-                            <a href="" >
-                                <i class="fal fa-chevron-left"></i>
-                                عسل آویشن و خواص آن
-                            </a>
-                        </li>
-                        <li>
-                            <a href="" >
-                                <i class="fal fa-chevron-left"></i>
-                                عسل آویشن و خواص آن
-                            </a>
-                        </li>
-                        <li>
-                            <a href="" >
-                                <i class="fal fa-chevron-left"></i>
-                                عسل آویشن و خواص آن
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <i class="fal fa-chevron-left"></i>
-                                عسل آویشن و خواص آن
-                            </a>
-                        </li>
-                        <li>
-                            <a href="" >
-                                <i class="fal fa-chevron-left"></i>
-                                عسل آویشن و خواص آن
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <i class="fal fa-chevron-left"></i>
-                                عسل آویشن و خواص آن
-                            </a>
-                        </li>
-                    </ul>
-                </section>
-                
-
-                <section class="item2">
-                    <p>باما همراه شوید</p>
-                    <ul>
-                        <li>
-                            <a href="" >
-                                <i class="fal fa-chevron-left"></i>
-                                تماس باما
-                            </a>
-                        </li>
-                        <li>
-                            <a href="" >
-                                <i class="fal fa-chevron-left"></i>
-                                درباره ما
-                            </a>
-                        </li>
-                        <li>
-                            <a href="" >
-                                <i class="fal fa-chevron-left"></i>
-                                پشتیبانی
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <i class="fal fa-chevron-left"></i>
-                                وبلاگ
-                            </a>
-                        </li>
-                    </ul>
-                </section>
-
-                <section class="namads">
-                    <p>نماد ها</p>
-                    <ul>
-                        <li>
-                            <a href="" >
-                                <img src="../assets/logo1.png" class="w-75" alt="">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="" >
-                                <img src="../assets/logo2.png" class="w-75" alt="">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="" >
-                                <img src="../assets/nashr.png" class="w-75" alt="">
-                            </a>
-                        </li>
-                    </ul>
-                </section>
-            </div>
-            
-        </div>
-    </div>
-    <div class="copy-right-footer">
         
-        <div class="items-copy-right-footer">
-            <div class="container">
-                <div class="copy-right-title">
-                    <small>&copy;  تمامی حقوق این وبسایت متعلق به شرکت چهل گیاه  است</small>
-                </div>
-                <div class="pm-resans">
-                    <ul class="social-network">
-                        <li><a href="#"><i class="fab fa-telegram-plane fa-2x"></i></a></li>
-                        <li><a href="#"><i class="fab fa-instagram fa-2x"></i></a></li>
-                        <li><a href="#"><i class="fab fa-youtube fa-2x"></i></a></li>
-                        <li><a href="#"><i class="fab fa-twitter fa-2x"></i></a></li>
-                        <li><a href="#"><i class="fab fa-facebook-square fa-2x"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-            
-        </div>
-    </div>
-    <!-- End Footer -->
-    <back-to-top></back-to-top>
+        <footer-vue></footer-vue>
+        <back-to-top></back-to-top>
     </main>
 </template>
 
 <script>
+import footerVue from './footer.vue'
 import BackToTop from './inden/back-to-top.vue'
 export default{
     data(){
@@ -561,8 +436,8 @@ export default{
     },
 
     components: {
-        BackToTop
-        
+        BackToTop,
+        footerVue,
     }
 
     
