6c18c7d6bbce6d729d9dd838fd470f345962d092
Milad Mohammadi <afraaliansite@gmail.com>
2020-12-21 12:20:40 -0800

    commit 3

--- src/components/views/productPage.vue
+++ src/components/views/productPage.vue
@@ -5,7 +5,13 @@
       <div class="gallery-product">
         <div class="gallery-product-item">
           <div class="gallery-product-image">
-            <img src="../../assets/image-product/download.jpg" alt="">
+            <img src="../../assets/image-product/download.jpg"  alt="">
+          </div>
+          <div class="gallery-product-image">
+            <img src="../../assets/image-product/عسل-آویشن-ممتاز-800-گرمی.jpg" alt="" style="margin: 0 -201% 0 0;">
+          </div>
+          <div class="gallery-product-image" style="margin: 0px -763px 0px 0px;">
+            <img src="../../assets/image-product/گرده-گل-مخلوط-150-گرمی.jpg" alt="">
           </div>
         </div>
         <ul class="gallery-product-items">
@@ -16,12 +22,12 @@
           </li>
           <li class="js-product-thumb-img">
             <div class="thumb-wrapper">
-              <img src="../../assets/image-product/download.jpg" alt="">
+              <img src="../../assets/image-product/عسل-آویشن-ممتاز-800-گرمی.jpg" alt="">
             </div>
           </li>
           <li class="js-product-thumb-img">
             <div class="thumb-wrapper">
-              <img src="../../assets/image-product/download.jpg" alt="">
+              <img src="../../assets/image-product/گرده-گل-مخلوط-150-گرمی.jpg" alt="">
             </div>
           </li>
           
