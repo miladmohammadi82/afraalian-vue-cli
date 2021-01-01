5b142ee82ff60c423f7a83b32d86b3463ef45bf7
Milad Mohammadi <afraaliansite@gmail.com>
2021-01-01 08:21:59 -0800

    commit 3

--- src/components/views/cartPage.vue
+++ src/components/views/cartPage.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="">
+  <div class="container container-cart-page">
+    <table class="content-table">
+      <thead>
+        <tr>
+          <th>1</th>
+          <th>2</th>
+          <th>3</th>
+          <th>4</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>cbhjd</td>
+          <td>cbhjd</td>
+          <td>cbhjd</td>
+          <td>cbhjd</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
