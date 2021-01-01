27fdd345bae92d23ed05f58d2cd7cb4915a357b3
Milad Mohammadi <afraaliansite@gmail.com>
2021-01-01 09:22:07 -0800

    commit 5

--- src/components/views/cartPage.vue
+++ src/components/views/cartPage.vue
@@ -3,19 +3,30 @@
     <table class="content-table">
       <thead>
         <tr>
-          <th>1</th>
-          <th>2</th>
-          <th>3</th>
-          <th>4</th>
+          <th>محصول</th>
+          <th>توضیحات</th>
+          <th>قسمت واحد</th>
+          <th>تعداد</th>
+          <th>حذف</th>
+          <th>مجموع</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>cbhjd</td>
-          <td>cbhjd</td>
-          <td>cbhjd</td>
-          <td>cbhjd</td>
+          <td>عکس</td>
+          <td>عسل کاکوتی ممتاز 400 گرمی</td>
+          <td>150,000 تومان</td>
+          <td>
+            <div class="number__container">
+              <button @click="delNumber" class="decrement"><i class="far fa-minus"></i></button>
+              <input v-model="numberProduct" type="text" inputmode="numeric" placeholder="تعداد" />
+              <button @click="addNumber" class="increment"><i class="far fa-plus"></i></button>
+            </div>
+          </td>
+          <td><i class="far fa-times fa-2x text-danger" style="cursor: pointer;"></i></td>
+          <td>150,000 تومان</td>
         </tr>
+        
       </tbody>
     </table>
   </div>
