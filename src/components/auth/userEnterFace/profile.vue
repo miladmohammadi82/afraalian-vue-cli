2b091a738c0f6784a590105060e0fadb15332af4
Milad Mohammadi <afraaliansite@gmail.com>
2021-01-04 10:38:15 -0800

    commit 1

---
+++ src/components/auth/userEnterFace/profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="content-page">
+    <div class="continer-profile-page">
+        <div class="sidebar-profile"></div>
+        <div class="continer-profile-page"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -5,12 +5,14 @@ import main from './components/main.vue';
 import cartPage from "./components/views/cartPage.vue";
 import registerPage from "./components/auth/register.vue"
 import loginPage from "./components/auth/login.vue"
+import profile from "./components/auth/userEnterFace/profile.vue"
 const routes = [
     { path: "/", component: main },
     { path: "/product", component: productPage },
     { path: "/shoping-cart", component: cartPage },
     { path: "/register", component: registerPage },
     { path: "/login", component: loginPage },
+    { path: "/profile", component: profile },
 ]
 
 
