4712f48834cf0492e4e6c224be42415dd5d3a5d2
Milad Mohammadi <afraaliansite@gmail.com>
2021-01-05 00:49:20 -0800

    complet sidebar

--- src/components/auth/userEnterFace/profile.vue
+++ src/components/auth/userEnterFace/profile.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="content-page">
     <div class="continer-profile-page">
-        <div class="sidebar-profile">
-            
+      <div class="sidebar-profile">
+        <div class="user-profile">
+          <div class="image-profile">
+            <img src="../../../assets/d41d8cd98f00b204e9800998ecf8427e-min.jpg" alt="">
+          </div>
+          <ul class="infos-user">
+            <li><h4>میلاد محمدی</h4></li>
+            <li><small>091165565212</small></li>
+          </ul>
         </div>
-        <div class="content-profile-page"></div>
+        <div class="pt-5 list-user-prodile-section">
+          <ul class="list-user-prodile-ul">
+            <li><a href="#"><i class="far fa-box-open fa-lg"></i>&nbsp;&nbsp;سفارش های من</a></li>
+            <li><a href="#"><i class="far fa-heart fa-lg"></i>&nbsp;&nbsp;علاقه مندی ها</a></li>
+            <li><a href="#"><i class="far fa-comments fa-lg"></i>&nbsp;&nbsp;نظرات</a></li>
+            <li><a href="#"><i class="far fa-route fa-lg"></i>&nbsp;&nbsp;نشانی</a></li>
+            <li><a href="#"><i class="far fa-user fa-lg"></i>&nbsp;&nbsp;اصلاعات حساب</a></li>
+            <li><a href="#"><i class="far fa-sign-out-alt fa-lg"></i>&nbsp;&nbsp;خروج از حساب</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="content-profile-page"></div>
     </div>
   </div>
 </template>
