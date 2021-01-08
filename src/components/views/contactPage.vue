5b7d2cb6990499826db2f337f279a3bbf4e6af09
Milad Mohammadi <afraaliansite@gmail.com>
2021-01-07 23:22:19 -0800

    commit 2

--- src/components/views/contactPage.vue
+++ src/components/views/contactPage.vue
@@ -1,4 +1,76 @@
 <template>
+
+<div class="container container-cart-page">
+    
+
+    <div class="contact-form-box">
+      <div class="item-mored-niaz-forPay">
+        <div class="addres-girande-box">
+          
+         
+            <div class="title-contact-page">
+                <h5>تماس با فلانی</h5>
+            </div>
+            
+            <div class="contact-form-filds ">
+                <form action="" class="row"> 
+                    <div class="form-group col-lg-6">
+                        <label for="">نام کاربری</label>
+                        <input type="text" class="mt-2 form-control" placeholder="نام کاربری" name="" id="">
+                    </div>
+                    <div class="form-group col-lg-6">
+                        <label for="">موضوع</label>
+                        <input type="text" class="mt-2 form-control" placeholder="موضوع" name="" id="">
+                    </div>
+                    <div class="form-group col-lg-6">
+                        <label for="">ایمیل</label>
+                        <input type="email" class="mt-2 form-control" placeholder="ایمیل" name="" id="">
+                    </div>
+                    <div class="form-group col-lg-6">
+                        <label for="">تلفن تماس</label>
+                        <input type="text" class="mt-2 form-control" placeholder="تلفن تماس" name="" id="">
+                    </div>
+
+                    <div class="form-group col-lg-12 ">
+                        <label for="">متن پیام</label>
+                        <textarea class="mt-2 form-control textarea-cantact-page" placeholder="متن پیام خود را بنویسید ..." name="" id="" cols="30" rows="10"></textarea>
+                    </div>
+
+                    <div class="btn-save-info-edit-user">
+                        <div class="btn btn-primary">ارسال</div>
+                    </div>
+                </form>
+            </div>
+          
+        </div>
+        
+        <div class="router-contact-this-web">
+         
+            <h5>راه های ارتباط باما</h5>
+            <div class="router-contact-items">
+                <ul>
+                    <li>
+                        <div class="icon-box-contact-page">
+                            <i class="fas fa-phone-alt fa-2x"></i>
+                        </div>
+                        <div class="title-contact-page">
+                            <h6>تماس بگیرید</h6>
+                            <small>021-4541521</small>
+                        </div>
+                    </li>
+                    
+                </ul>
+            </div>
+                
+            
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+<!-- 
   <div class="container container-contact-box">
     
     <div class="content-contact-page">
@@ -55,7 +127,10 @@
 
         </div>
     </div>
-  </div>
+  </div> -->
+
+
+
 </template>
 
 <script>
